89d45b24ac835f15592d23d521eb9523c4a924c8
Antonia <antonianomamiukor@gmail.com>
2020-06-08 23:49:09 -0700

    added setup, mock-auth-hash, and perform_login methods to test_helper. created fixtures

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -23,4 +23,34 @@ class ActiveSupport::TestCase
   # Setup all fixtures in test/fixtures/*.yml for all tests in alphabetical order.
   fixtures :all
   # Add more helper methods to be used by all tests here...
+
+  def setup
+    OmniAuth.config.test_mode = true
+  end
+
+  def mock_auth_hash(user)
+    return {
+      provider: user.provider,
+      uid: user.uid,
+      info: {
+        email: user.email,
+        nickaname: user.name
+      },
+    }
+  end
+
+  #PERFORMS A LOG-IN WITH EITHER A PASSED-IN USER OR THE FIRST TEST USER
+  def perform_login(user = nil)
+    user ||= User.first
+    OmniAuth.config.mock_auth[:github] = OmniAuth::AuthHash.new(mock_auth_hash(user))
+
+    #Act Try to call the callback route
+    get auth_callback_path(:github)
+
+    #Verify user ID was saved. if not- test is invalid
+    expect(session[:user_id]).must_equal user.id
+  
+    return user
+  end
+
 end
